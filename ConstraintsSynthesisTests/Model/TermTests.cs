ab89bc84295cfe04ef53d359d531e0f5211bd7e0
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-03-12 22:41:17 +0100

    tests: add tests for ToString() in Term

--- ConstraintsSynthesisTests/Model/TermTests.cs
+++ ConstraintsSynthesisTests/Model/TermTests.cs
@@ -1,4 +1,6 @@
-﻿using ConstraintsSynthesis.Model;
+﻿using System;
+using System.Collections.Generic;
+using ConstraintsSynthesis.Model;
 using NUnit.Framework;
 
 namespace ConstraintsSynthesisTests.Model
@@ -12,20 +14,72 @@ namespace ConstraintsSynthesisTests.Model
             _term = new Term();
         }
 
-        private static readonly object[] PositiveAndNegativeLabelsCountCases =
+        private static readonly object[] ValueInValidPointTestCases =
         {
-            new object[] {3127, 518},
-            new object[] {1085, 2504}
+            new object[] {new Dictionary<int, double>(), new Point(new[] {1.0, 2.0}), 0.0},
+            new object[] {new Dictionary<int, double> {{0, 3.0}}, new Point(new[] {2.0, 2.0}), 8.0},
+            new object[]
+                {new Dictionary<int, double> {{0, 3.0}, {1, 2.0}, {2, 1.0}}, new Point(new[] {2.0, 2.0, 3.0}), 96.0},
+            new object[]
+                {new Dictionary<int, double> {{0, 3.0}, {1, 2.0}, {2, 1.0}}, new Point(new[] {0.0, 0.0, 0.0}), 0.0}
+        };
+
+        private static readonly object[] ValueInInvalidPointTestCases =
+        {
+            new object[] {new Dictionary<int, double> {{0, 3.0}, {1, 2.0}, {2, 1.0}}, new Point(new[] {2.0, 2.0})},
+            new object[] {new Dictionary<int, double> {{0, 1.0}, {1, 1.0}, {3, 1.0}}, new Point(new[] {2.0, 2.0, 2.0})}
+        };
+
+        private static readonly object[] ToStringTestCases =
+        {
+            new object[] {new Dictionary<int, double> {{0, 3.0}, {1, 2.0}, {2, 1.0}}, "x0^3 x1^2 x2^1"},
+            new object[] {new Dictionary<int, double> {{0, 1.0}, {1, 1.0}, {3, 1.0}}, "x0^1 x1^1 x3^1"},
+            new object[] {new Dictionary<int, double> {{0, 1.5}, {1, 1.2}, {3, -10.2}}, "x0^1.5 x1^1.2 x3^-10.2"},
+            new object[] {new Dictionary<int, double> {{0, 1.5}, {1, 0}, {3, -10.2}}, "x0^1.5 x3^-10.2"},
+            new object[] {new Dictionary<int, double> {{0, 1.5}, {3, -10.2}, {1, 2}}, "x0^1.5 x1^2 x3^-10.2"}
         };
 
         private Term _term;
 
         [Test]
-        public void GetVariableThatIsNotPartOfTheTermTests()
+        public void GetVariableThatIsNotPartOfTheTermTest()
         {
             var exponent = _term[0];
 
             Assert.AreEqual(0, exponent, double.Epsilon);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ToStringTestCases))]
+        public void ToStringTest(Dictionary<int, double> variables, string expected)
+        {
+            foreach (var entry in variables)
+                _term[entry.Key] = entry.Value;
+
+
+            Assert.AreEqual(expected, _term.ToString());
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ValueInInvalidPointTestCases))]
+        public void ValueInInvalidPointTest(Dictionary<int, double> variables, Point point)
+        {
+            foreach (var entry in variables)
+                _term[entry.Key] = entry.Value;
+
+            Assert.Throws(typeof(IndexOutOfRangeException), () => _term.Value(point));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ValueInValidPointTestCases))]
+        public void ValueInValidPointTest(Dictionary<int, double> variables, Point point, double expectedValue)
+        {
+            foreach (var entry in variables)
+                _term[entry.Key] = entry.Value;
+
+            var actualValue = _term.Value(point);
+
+            Assert.AreEqual(expectedValue, actualValue, double.Epsilon);
         }
     }
 }
