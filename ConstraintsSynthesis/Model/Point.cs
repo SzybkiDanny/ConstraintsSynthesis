2464c26d29129696684109cd27548f61e625cb14
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-03-16 11:14:43 +0100

    feature: add < and > operators to Point class

--- ConstraintsSynthesis/Model/Point.cs
+++ ConstraintsSynthesis/Model/Point.cs
@@ -1,4 +1,7 @@
-﻿namespace ConstraintsSynthesis.Model
+﻿using System;
+using System.Linq;
+
+namespace ConstraintsSynthesis.Model
 {
     public class Point
     {
@@ -20,6 +23,30 @@
         public Point(double[] coordinates)
         {
             Coordinates = coordinates;
+        }
+
+        public static bool operator <(Point p1, Point p2)
+        {
+            if (p1.Coordinates.Length != p2.Coordinates.Length)
+                throw new ArgumentException("Both points need to have the same dimensions count");
+
+            for (int i = 0; i < p1.Coordinates.Length; i++)
+                if (p1[i] >= p2[i])
+                    return false;
+
+            return true;
+        }
+
+        public static bool operator >(Point p1, Point p2)
+        {
+            if (p1.Coordinates.Length != p2.Coordinates.Length)
+                throw new ArgumentException("Both points need to have the same dimensions count");
+
+            for (int i = 0; i < p1.Coordinates.Length; i++)
+                if (p1[i] <= p2[i])
+                    return false;
+
+            return true;
         }
 
         public override string ToString()
