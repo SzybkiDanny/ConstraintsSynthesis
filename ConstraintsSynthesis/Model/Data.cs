5529c3d2bb13e5e34ba0018ca8d37a872dcb5e15
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-03-18 10:36:35 +0100

    feature: add marginal points generation

---
+++ ConstraintsSynthesis/Model/Data.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MathNet.Numerics.Random;
+
+namespace ConstraintsSynthesis.Model
+{
+    public class Data
+    {
+        private readonly MersenneTwister _random = new MersenneTwister();
+
+        public List<Point> Points { get; } = new List<Point>();
+
+        public void Load(string filename, string delimiter = "")
+        {
+            var data = new List<Point>();
+
+            using (var file = new StreamReader(filename))
+            {
+                while (!file.EndOfStream)
+                {
+                    var values =
+                        file.ReadLine()?.Split(new[] {delimiter}, StringSplitOptions.None).Select(
+                            double.Parse);
+                    var point = new Point(values.Take(values.Count() - 1).ToArray())
+                    {
+                        Label = Math.Abs(values.Last() - 1.0) < double.Epsilon
+                    };
+
+                    data.Add(point);
+                }
+            }
+
+            Points.AddRange(data);
+        }
+
+        public List<Point> FindMarginalPoints(int count)
+        {
+            var result = new List<Point>();
+            var consideredPoints = new List<Point>(Points);
+
+            for (int i = 0; i < count; i++)
+            {
+                var randomPoint = GetRandomPoint();
+                var maxDistance = 0.0;
+                Point marginalPoint = null;
+
+                foreach (var point in consideredPoints.Where(p => p.Label))
+                {
+                    var distance = CalculateDistanceBetweenPoints(randomPoint, point);
+                    if (distance <= maxDistance)
+                        continue;
+
+                    maxDistance = distance;
+                    marginalPoint = point;
+                }
+ 
+                consideredPoints.Remove(marginalPoint);
+                result.Add(marginalPoint);
+            }
+
+            return result;
+        }
+
+        private Point GetRandomPoint() => 
+            Points[_random.Next(Points.Count)];
+
+        private double CalculateDistanceBetweenPoints(Point p1, Point p2) =>
+            Math.Sqrt(p1.Coordinates.Zip(p2.Coordinates, (d1, d2) => Math.Pow(d1 - d2, 2)).Sum());
+    }
+}
