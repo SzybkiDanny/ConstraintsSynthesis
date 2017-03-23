22d20282f632a222566618be4a2519ed967fc999
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-03-23 11:07:37 +0100

    feature: add LinearConstraint class

---
+++ ConstraintsSynthesis/Model/LinearConstraint.cs
@@ -0,0 +1,15 @@
+﻿namespace ConstraintsSynthesis.Model
+{
+    public class LinearConstraint : Constraint
+    {
+        public LinearConstraint(double[] coefficients, double absoluteTerm)
+        {
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                Terms[new Term() { [i] = 1.0 }] = coefficients[i];
+            }
+
+            AbsoluteTerm = absoluteTerm;
+        }
+    }
+}
