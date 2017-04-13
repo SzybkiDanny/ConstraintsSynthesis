2b90e78537cdf200ba61aca0ec434c38cc17b0ac
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-04-13 11:10:51 +0200

    feature: add X-means algorithm implementation

---
+++ ConstraintsSynthesis/Algorithm/XMeans.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Accord.MachineLearning;
+using Accord.Math;
+using Accord.Statistics.Distributions.Univariate;
+using ConstraintsSynthesis.Model;
+
+namespace ConstraintsSynthesis.Algorithm
+{
+    public class XMeans
+    {
+        public readonly int MinK;
+        public IList<Cluster> Clusters { get; } = new List<Cluster>();
+
+        public XMeans(int minK = 2)
+        {
+            MinK = minK;
+        }
+
+        public void Fit(IList<Point> points)
+        {
+            var clusters = SplitPointsIntoClusters(points, MinK);
+
+            RecursivelySplit(clusters);
+        }
+
+        private void RecursivelySplit(IEnumerable<Cluster> clusters)
+        {
+            foreach (var cluster in clusters)
+            {
+                if (cluster.Size <= 3)
+                {
+                    Clusters.Add(cluster);
+                    continue;
+                }
+
+                var splitClusters = SplitPointsIntoClusters(cluster.Points, 2);
+                var c1 = splitClusters[0];
+                var c2 = splitClusters[1];
+
+                var beta = c1.Centroid.Subtract(c2.Centroid).Euclidean()/
+                           Math.Sqrt(c1.Covarianve.PseudoDeterminant() +
+                                     c2.Covarianve.PseudoDeterminant());
+                var alpha = 0.5/NormalDistribution.Standard.DistributionFunction(beta);
+                var bic = -2*
+                          (cluster.Size*Math.Log(alpha) + c1.LogLikelihood +
+                           c2.LogLikelihood) + 2*cluster.K*Math.Log(cluster.Size);
+
+                if (bic < cluster.BIC)
+                    RecursivelySplit(splitClusters);
+                else
+                    Clusters.Add(cluster);
+            }
+        }
+
+        private static IList<Cluster> SplitPointsIntoClusters(IList<Point> points, int clustersCount)
+        {
+            var observations = points.Select(p => p.Coordinates).ToArray();
+            var kmeans = new KMeans(clustersCount);
+            var initialClustersIndices = kmeans.Learn(observations).Decide(observations);
+            var distinctIndices = initialClustersIndices.Distinct();
+            var clusters =
+                distinctIndices.Select(
+                    clusterIndex =>
+                        points.Where((p, index) => 
+                            initialClustersIndices[index] == clusterIndex).ToList())
+                    .Select((newClusterPoints, index) => 
+                        new Cluster(newClusterPoints, kmeans, index)).ToList();
+
+            return clusters;
+        }
+    }
+}
---
+++ ConstraintsSynthesis/Model/Cluster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Accord.MachineLearning;
+using Accord.Math;
+using Accord.Statistics;
+using Accord.Statistics.Distributions.Multivariate;
+
+namespace ConstraintsSynthesis.Model
+{
+    public class Cluster
+    {
+        private readonly MultivariateNormalDistribution _multivariateNormalDistribution;
+        public readonly int K;
+
+        public int Size => Points.Count;
+        public List<Point> Points { get; }
+        public double[] Centroid { get; }
+        public double[][] Covarianve { get; }
+
+        public Cluster(List<Point> points, KMeans kmeans, int index)
+        {
+            Points = points;
+            Centroid = kmeans.Clusters[index].Centroid;
+            Covarianve = kmeans.Clusters[index].Covariance;
+
+            var d = points.First().Coordinates.Length;
+            K = d*(d + 3)/2;
+
+            var pointsMatrix = points.Select(p => p.Coordinates).ToArray();
+            var means = pointsMatrix.Mean(pointsMatrix.Transpose().Select(x => x.Sum()).ToArray());
+            var covariance = pointsMatrix.Covariance(means);
+
+            _multivariateNormalDistribution = new MultivariateNormalDistribution(means, covariance);
+        }
+
+        public double LogLikelihood =>
+            Points.Sum(p =>
+                _multivariateNormalDistribution.LogProbabilityDensityFunction(p.Coordinates));
+
+        public double BIC =>
+            -2*LogLikelihood + K*Math.Log(Size);
+
+    }
+}
