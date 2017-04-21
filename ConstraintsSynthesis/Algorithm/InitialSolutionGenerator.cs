02bb046b5cf9d24ff5c14c810342534dee78bc06
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-04-21 16:46:33 +0200

    refactor: rename InitialSolution to InitialSolutionGenerator and make it static

---
+++ ConstraintsSynthesis/Algorithm/InitialSolutionGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ConstraintsSynthesis.Model;
+
+namespace ConstraintsSynthesis.Algorithm
+{
+    public static class InitialSolutionGenerator
+    {
+        public static IEnumerable<LinearConstraint> GenerateInitialConstraints(IList<Point> data)
+        {
+            var dimensions = data.First().Coordinates.Length;
+
+            for (var i = 0; i < dimensions; i++)
+            {
+                var min = data.Min(p => p[i]);
+                var minConstraint = new LinearConstraint(new Dictionary<int, double>() { {i, 1.0} }, dimensions, min)
+                {
+                    Sign = Inequality.GreaterThanOrEqual
+                };
+
+                yield return minConstraint;
+
+                var max = data.Max(p => p[i]);
+                var maxConstraint = new LinearConstraint(new Dictionary<int, double>() { {i, 1.0} }, dimensions, max);
+
+                yield return maxConstraint;
+            }
+        }
+    }
+}
