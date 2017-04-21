c78bb0c8511bd398740cd5ff1b02d8c9ed9319f1
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-04-21 16:38:40 +0200

    feature: add constraint squeezing to ConstraintLocalOptimization

--- ConstraintsSynthesis/Algorithm/ConstraintLocalOptimization.cs
+++ ConstraintsSynthesis/Algorithm/ConstraintLocalOptimization.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ConstraintsSynthesis.Model;
+using MathNet.Numerics.Random;
 
 namespace ConstraintsSynthesis.Algorithm
 {
     public class ConstraintLocalOptimization
     {
+        private readonly MersenneTwister _random = new MersenneTwister();
         private List<Point> PositivePoints { get; }
         private List<Point> NotSatisfiedPoints =>
             PositivePoints.Where(p => !Constraint.IsSatisfying(p)).ToList();
 
         public Constraint Constraint { get; }
         public List<Point> Points { get; }
-        public const double CoefficientOptimizationStep = 0.2;
+        public const double CoefficientOptimizationStep = 0.005;
 
         public int SatisfiedPointsCount =>
             PositivePoints.Count(Constraint.IsSatisfying);
@@ -24,13 +26,16 @@ namespace ConstraintsSynthesis.Algorithm
             PositivePoints = Points.Where(p => p.Label).ToList();
         }
 
-        public void Optimize(bool optimizeSign = true, bool optimizeCoefficients = true)
+        public void Optimize(bool optimizeSign = true, bool optimizeCoefficients = true, bool squeezeConstraint = true)
         {
             if (optimizeSign && SatisfiedPointsCount*2 < PositivePoints.Count)
                 Constraint.InvertInequalitySing();
 
             if (optimizeCoefficients)
                 OptimizeCoefficients();
+
+            if (squeezeConstraint)
+                SqueezeConstraint();
 
         }
 
@@ -54,6 +59,31 @@ namespace ConstraintsSynthesis.Algorithm
                 }
 
                 Constraint[selectedTerm] += CoefficientOptimizationStep*stepSign;
+            }
+        }
+
+        private void SqueezeConstraint()
+        {
+            var termsToSqueeze = new List<Term>(Constraint.Terms.Keys);
+            var stepSign = Constraint.Sign == Inequality.LessThanOrEqual ? 1 : -1;
+
+            Constraint.AbsoluteTerm += 20 * stepSign;
+
+            while (termsToSqueeze.Count > 0)
+            {
+                var selectedIndex = _random.Next(termsToSqueeze.Count);
+                var selectedTerm = termsToSqueeze[selectedIndex];
+                var testResult = TestCoefficientChange(selectedTerm,
+                    CoefficientOptimizationStep*stepSign);
+
+                if (testResult == 0)
+                {
+                    Constraint[selectedTerm] += CoefficientOptimizationStep*stepSign;
+                }
+                else
+                {
+                    termsToSqueeze.RemoveAt(selectedIndex);
+                }
             }
         }
 
