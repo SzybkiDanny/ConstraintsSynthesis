8c447f6ec9ab0e94900414d4bda581127895e2fd
SzybkiDanny <SzybkiDanny@users.noreply.github.com>
2017-04-21 16:44:07 +0200

    feature: add another constructor that accepts dictionary of terms to LinearConstraint class and implementation of IClonable

--- ConstraintsSynthesis/Model/LinearConstraint.cs
+++ ConstraintsSynthesis/Model/LinearConstraint.cs
@@ -1,15 +1,45 @@
-﻿namespace ConstraintsSynthesis.Model
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConstraintsSynthesis.Model
 {
     public class LinearConstraint : Constraint
     {
         public LinearConstraint(double[] coefficients, double absoluteTerm)
         {
-            for (int i = 0; i < coefficients.Length; i++)
+            for (var i = 0; i < coefficients.Length; i++)
             {
                 Terms[new Term() { [i] = 1.0 }] = coefficients[i];
             }
 
             AbsoluteTerm = absoluteTerm;
+        }
+
+        public LinearConstraint(Dictionary<int, double> coefficients, int dimensions, double absoluteTerm)
+        {
+            for (var i = 0; i < dimensions; i++)
+            {
+                Terms[new Term() {[i] = 1.0}] = coefficients.Keys.Contains(i)
+                    ? coefficients[i]
+                    : 0;
+            }
+
+            AbsoluteTerm = absoluteTerm;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                var term = new Term() {[index] = 1.0};
+                return Terms.ContainsKey(term) ? Terms[term] : 0;
+            }
+            set { Terms[new Term() { [index] = 1.0 }] = value; }
+        }
+
+        public override object Clone()
+        {
+            return new LinearConstraint(Terms.Values.ToArray(), AbsoluteTerm) {Sign = Sign};
         }
     }
 }
